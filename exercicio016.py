2a57c0a151bb88d2ee3430abfa470b50ad42917d
Guilherme Rocha <gulhermebr@gmail.com>
2022-04-04 10:49:45 -0300

    exercicio porção inteira

--- exercicio016.py
+++ exercicio016.py
@@ -1,4 +1,3 @@
-km_percorridos = float(input('Digite quantos Km foram percorridos com o carro: '))
-dias_alugados = int(input('Digite quantos dias o carro foi alugado: '))
-valor_total = (dias_alugados * 60) + (km_percorridos * 0.15)
-print(f'O preço total do aluguel do carro será de {valor_total}.')
+from math import trunc
+número = float(input('Digite um número: '))
+print(f'O número digitado foi {número}, e a sua porção inteira é {trunc(número)}')
