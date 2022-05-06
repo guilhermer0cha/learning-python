7e8c948639bb57b5fd62e8a009df620665ed3f2c
Guilherme Rocha <gulhermebr@gmail.com>
2022-05-06 10:28:23 -0300

    comparador de números

---
+++ exercicio038.py
@@ -0,0 +1,8 @@
+number1 = int(input('Primeiro número: '))
+number2 = int(input('Segundo número: '))
+if number1 > number2:
+    print('O PRIMEIRO valor é maior!')
+if number1 < number2:
+    print('O SEGUNDO valor é maior!')
+else:
+    print('Os dois valores são IGUAIS!')
