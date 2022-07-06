4a6e0ac4e5d05e23c8f2f09d93961630b34351f9
Guilherme Rocha <gulhermebr@gmail.com>
2022-07-06 10:41:19 -0300

    lista ordenada sem repetiçoes

--- exercises/exercicio078.py
+++ exercises/exercicio078.py
@@ -1,21 +1,21 @@
 valores = []
 maior = 0
 menor = 0
-for n in range(0, 5):
-    valores.append(int(input(f'Digite um valor para a posição {n}: ')))
-    if n == 0:
-        maior = menor = valores[n]
+for c in range(0, 5):
+    valores.append(int(input('Digite um valor: ')))
+    if c == 0:
+        maior = menor = valores[c]
     else:
-        if valores[n] > maior:
-            maior = valores[n]
-        elif valores[n] < menor:
-            menor = valores[n]
+        if valores[c] > maior:
+            maior = valores[c]
+        elif valores[c] < menor:
+            menor = valores[c]
 print(f'Você digitou os valores {valores}')
 print(f'O menor valor digitado foi {menor} nas posições ', end='')
 for i, v in enumerate(valores):
     if v == menor:
-        print(f'{i}... ', end='')
+        print(f'{i}...', end='')
 print(f'\nO maior valor digitado foi {maior} nas posições ', end='')
 for i, v in enumerate(valores):
     if v == maior:
-        print(f'{i}... ', end='')
+        print(f'{i}...', end='')
--- exercises/exercicio079.py
+++ exercises/exercicio079.py
@@ -1,13 +1,13 @@
-números = []
+lista = []
 while True:
     n = int(input('Digite um valor: '))
-    if n not in números:
-        números.append(n)
-        print('Valor adicionado com sucesso')
+    if n not in lista:
+        lista.append(n)
+        print('Valor adicionado com sucesso...')
     else:
-        print('Valor duplicado. Não vou adicionar...')
-    r = input('Quer continuar? [S/N] ').upper()
-    if r == 'N':
+        print('Valor duplicado! Não vou adicionar...')
+    r = input('Você deseja continuar? [S/N] ')
+    if r in 'nN':
         break
-números.sort()
-print(f'Você digitou os números {números}')
+lista.sort()
+print(f'Você digitou os valores {lista}')
