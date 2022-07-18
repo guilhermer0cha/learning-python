d4bc942eabb23c09acdf673764637a338c6e05bd
Guilherme Rocha <gulhermebr@gmail.com>
2022-07-18 14:29:33 -0300

    sorteios da mega sena

--- exercises/exercicio088.py
+++ exercises/exercicio088.py
@@ -5,8 +5,9 @@ contador = 0
 njogos = int(input('Quantos jogos você quer que eu sorteie? '))
 for j in range(1, njogos+1):
     for n in range(0, 6):
-        números = random.randint(0, 60)
-        sorteio.append(números)
+        números = random.randint(1, 60)
+        if números not in sorteio:
+            sorteio.append(números)
         contador += 1
     print(f'Jogo {j}: {sorteio}')
     sorteio.clear()
