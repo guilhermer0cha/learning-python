5bb1767db6640a051ae62e1f50bad5287a39a44f
Guilherme Rocha <gulhermebr@gmail.com>
2022-07-19 15:41:11 -0300

    palpites para a mega sena

--- exercises/exercicio088.py
+++ exercises/exercicio088.py
@@ -1,14 +1,12 @@
 import random
 
 sorteio = []
-contador = 0
 njogos = int(input('Quantos jogos você quer que eu sorteie? '))
 for j in range(1, njogos+1):
     for n in range(0, 6):
         números = random.randint(1, 60)
         if números not in sorteio:
             sorteio.append(números)
-        contador += 1
     print(f'Jogo {j}: {sorteio}')
     sorteio.clear()
 
