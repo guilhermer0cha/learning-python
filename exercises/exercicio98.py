67ee7f5746adf9684a8586e349d646065ce4b4da
Guilherme Rocha <gulhermebr@gmail.com>
2022-10-03 14:04:44 -0300

    função que descobre o maior

--- exercises/exercicio98.py
+++ exercises/exercicio98.py
@@ -1,21 +1,25 @@
-import time
+from time import sleep
 
 def contador(início, fim, passo):
     print(f'Contagem de {início} até {fim} de {passo} em {passo}')
     if início > fim: 
         for c in range(início, fim-1, -passo):
-            print(c, end=' ')
+            print(c, end=' ', flush=True)
+            sleep(0.3)
     for c in range(início, fim+1, passo):
-        print(c, end=' ')
+        print(c, end=' ', flush=True)
+        sleep(0.3)
 print('=' * 30)
 print('Contagem de 1 até 10 de 1 em 1')
 for c in range(0, 10):
-    print(c+1, end=' ')
+    print(c+1, end=' ', flush=True)
+    sleep(0.3)
 print('FIM!')
 print('=' * 30)
 print('Contagem de 10 até 0 de 2 em 2')
 for c in range(10, -2, -2):
-    print(c, end=' ')
+    print(c, end=' ', flush=True)
+    sleep(0.3)
 print('FIM!')
 print('=' * 30)
 print('Agora é sua vez de personalizar a contagem!')
