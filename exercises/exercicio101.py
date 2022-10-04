121d89ffa59e6cd91181586cc0ffc2b7df89025a
Guilherme Rocha <gulhermebr@gmail.com>
2022-10-04 13:30:59 -0300

    funçoes para votação

---
+++ exercises/exercicio101.py
@@ -0,0 +1,13 @@
+def voto(votos):
+    idade = 2022 - anoNascimento
+    if idade >= 18:
+        print(f'Com {idade} anos: VOTO OBRIGATÓRIO.')
+    else:
+        if idade == 16 or idade == 17:
+            print(f'Com {idade} anos: VOTO OPCIONAL.')
+        else:
+            print(f'Com {idade} anos: NÃO VOTA.')
+
+
+anoNascimento = int(input('Em que ano você nasceu? '))
+voto(anoNascimento)
